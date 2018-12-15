96e09d0506ec0a1371ce684b0a51a13af799d2ee
Kim Sungsik <sskim0126@kookmin.ac.kr>
2018-12-15 22:00:10 +0900

    카테고리 화면, 인기상품 화면 추가 / 메인화면에서 카테고리 화면으로 넘어갈수 있도록 함

---
+++ app/src/main/java/com/kmu/footballshop/GlovesActivity.java
@@ -0,0 +1,12 @@
+package com.kmu.footballshop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GlovesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_boots);
+    }
+}
--- app/src/main/java/com/kmu/footballshop/MainActivity.java
+++ app/src/main/java/com/kmu/footballshop/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kmu.footballshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Football Shop");
+
+        // 검색 기능
+        final EditText itemName = (EditText) findViewById(R.id.searchInput);
+        ImageButton enterButton = (ImageButton) findViewById(R.id.searchButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchContents = itemName.getText().toString().toUpperCase();
+                Intent intent;
+                switch (searchContents) {
+                    case "BOOTS":
+                        intent = new Intent(getApplicationContext(), BootsActivity.class);
+                        startActivity(intent);
+                        break;
+                    case "SHIRT":
+                        intent = new Intent(getApplicationContext(), ShirtActivity.class);
+                        startActivity(intent);
+                        break;
+                    case "GLOVES":
+                        intent = new Intent(getApplicationContext(), GlovesActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+
+        // 카테고리 기능
+        ImageButton bootsButton = (ImageButton) findViewById(R.id.bootsButton);
+        bootsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BootsActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton shirtButton = (ImageButton) findViewById(R.id.shirtButton);
+        bootsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ShirtActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton glovesButton = (ImageButton) findViewById(R.id.glovesButton);
+        bootsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GlovesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
