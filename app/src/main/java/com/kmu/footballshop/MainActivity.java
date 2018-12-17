52c9dcfd61ea30f1597b5bb1220717a7dda42bb3
Kim Sungsik <sskim0126@kookmin.ac.kr>
2018-12-18 02:43:36 +0900

    Hot Items 액티비티 삭제, 메인 타이틀 수정

--- app/src/main/java/com/kmu/footballshop/MainActivity.java
+++ app/src/main/java/com/kmu/footballshop/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Football Shop");
 
         // 검색 기능
         final EditText itemName = (EditText) findViewById(R.id.searchInput);
