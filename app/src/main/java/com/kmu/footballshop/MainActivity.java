a1ecc93b588d1836324b0340ac1aec8bc8faff4f
Kim Sungsik <sskim0126@kookmin.ac.kr>
2018-12-18 12:31:43 +0900

    toUpperCase를 빼먹어 검색 기능에서 에러가 난 것 수정

--- app/src/main/java/com/kmu/footballshop/MainActivity.java
+++ app/src/main/java/com/kmu/footballshop/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
         // 검색내용 복구
         searchInput = (EditText) findViewById(R.id.searchInput);
-        searchContents = searchInput.getText().toString();
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         searchContents = settings.getString("searchContents", "");
         searchInput.setText(searchContents);
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent;
+                searchContents = searchInput.getText().toString().toUpperCase();
                 switch (searchContents) {
                     case "BOOTS":
                         intent = new Intent(getApplicationContext(), BootsActivity.class);
